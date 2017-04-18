5d7066580aeb8d9a2c9da117995b9ab71fce0bd2
JamesBrofos <jamesbrofos@gmail.com>
2017-04-17 21:03:58 -0400

    mitre endpoint

--- thor_client/base.url.R
+++ thor_client/base.url.R
@@ -1,4 +1,5 @@
 base.url <- function(endpoint) {
-  url <- paste("http://127.0.0.1:5000/api/", endpoint, "/", sep="")
+  ## url <- paste("http://127.0.0.1:5000/api/", endpoint, "/", sep="")
+  url <- paste("http://aa-lnx01.mitre.org:5000/api/", endpoint, "/", sep="")
   return(url)
 }
